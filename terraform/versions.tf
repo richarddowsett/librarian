fcaa33522a70cdb548be92cbe05550f0bd406de4
Richard Dowsett <Richard.Dowsett@Siriusxm.com>
2026-07-27 21:23:13 +0100

    style(terraform): format HCL files with terraform fmt

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket         = "librarian-terraform-state"
-    key            = "terraform/state/terraform.tfstate"
-    region         = "eu-central-1"
+    bucket       = "librarian-terraform-state"
+    key          = "terraform/state/terraform.tfstate"
+    region       = "eu-central-1"
     use_lockfile = true
-    encrypt        = true
+    encrypt      = true
   }
 
   required_providers {
